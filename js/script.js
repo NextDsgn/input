88414d673b4cb93f213f64eb3d44d4c313e61815
Vlad <next.dzgn@gmail.com>
2020-07-18 19:47:56 +0300

    media

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const signUpBtn = document.querySelector('#signUpBtn');
+const signInBtn = document.querySelector('#signInBtn');
+const container = document.querySelector('.container');
+
+signUpBtn.addEventListener('click', () => {
+    container.classList.add('sign-up-mode')
+});
+
+signInBtn.addEventListener('click', () => {
+    container.classList.remove('sign-up-mode')
+});
